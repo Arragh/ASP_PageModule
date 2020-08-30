9fd9d9cdce90e15b8a925dbc22fe11b295d022bd
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2020-08-31 00:44:00 +0300

    Создание и просмотр страниц. Пока без проверок, валидации и прочих ништяков.

--- ASP_PageModule/Controllers/PageController.cs
+++ ASP_PageModule/Controllers/PageController.cs
@@ -6,6 +6,7 @@ using ASP_PageModule.Models.Page;
 using ASP_PageModule.Models.Service;
 using ASP_PageModule.ViewModels.Page;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASP_PageModule.Controllers
 {
@@ -17,33 +18,66 @@ namespace ASP_PageModule.Controllers
             cmsDB = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Page> pages = await cmsDB.Pages.ToListAsync();
+            Dictionary<Guid, string> model = new Dictionary<Guid, string>();
+            foreach (var page in pages)
+            {
+                model.Add(page.Id, page.PageTitle);
+            }
+            ViewBag.Pages = model;
+
+            return View(model);
         }
 
+        #region Создание страницы [GET]
         [HttpGet]
         public IActionResult AddPage()
         {
             return View();
         }
+        #endregion
 
+        #region Создание страницы [POST]
         [HttpPost]
         public async Task<IActionResult> AddPage(AddPageViewModel model)
         {
-            Page page = new Page()
+            if (ModelState.IsValid)
             {
-                Id = Guid.NewGuid(),
-                PageTitle = model.PageTitle,
-                PageBody = model.PageBody,
-                PageDate = DateTime.Now,
-                UserName = "Mnemonic" // Хардкод. Потом обязательно заменить !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                Page page = new Page()
+                {
+                    Id = Guid.NewGuid(),
+                    PageTitle = model.PageTitle,
+                    PageBody = model.PageBody,
+                    PageDate = DateTime.Now,
+                    UserName = "Mnemonic" // Хардкод. Потом обязательно заменить !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                };
+
+                await cmsDB.Pages.AddAsync(page);
+                await cmsDB.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Page");
+            }
+
+            return View(model);
+        }
+        #endregion
+
+        #region Просмотр страницы
+        public async Task<IActionResult> ViewPage(Guid pageId)
+        {
+            Page page = await cmsDB.Pages.FirstAsync(p => p.Id == pageId);
+
+            ViewPageViewModel model = new ViewPageViewModel()
+            {
+                PageTitle = page.PageTitle,
+                PageBody = page.PageBody
             };
 
-            await cmsDB.Pages.AddAsync(page);
-            await cmsDB.SaveChangesAsync();
-
-            return RedirectToAction("Index", "Page");
+            return View(model);
         }
+        #endregion
+
     }
 }
---
+++ ASP_PageModule/ViewModels/Page/ViewPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_PageModule.ViewModels.Page
+{
+    public class ViewPageViewModel
+    {
+        public string PageTitle { get; set; }
+        public string PageBody { get; set; }
+    }
+}
